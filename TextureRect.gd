1d5e851b64628355f7fac6e30fb7b219c5b0f72c
AndyLouisM <160281937+AndyLouisM@users.noreply.github.com>
2024-03-22 18:04:56 -0400

    cursor stuff

--- TextureRect.gd
+++ TextureRect.gd
@@ -11,6 +11,8 @@ func _ready():
 func _process(_add_constant_torquedelta):	
 	mouse_position = get_viewport().get_mouse_position()
 	self.position = mouse_position + Vector2(-30, -25)
+	var min = Vector2(0, 0)
+	var max = Vector2(1152, 648)
 	
 	pass
 	
