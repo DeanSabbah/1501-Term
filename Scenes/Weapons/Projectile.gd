1fb0ebfb0d70df6b401b4d28add313e43ce06ab6
AndyLouisM <160281937+AndyLouisM@users.noreply.github.com>
2024-03-31 20:33:38 -0400

    ADDED PROJECTILE COLLISONS, MAP 2 WORK. AND  ENEMY  SPRITE

--- Scenes/Weapons/Projectile.gd
+++ Scenes/Weapons/Projectile.gd
@@ -14,3 +14,21 @@ func _process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+
+func _on_body_entered(body:Node2D):
+
+
+	var map1_node = get_node(get_parent().get_node("Industrial").get_path())
+	var map2_node = get_node(get_parent().get_node("Exclusion").get_path())
+
+	
+	var tilemap1 = map1_node.get_node("Industiral")
+	var tilemap2 = map2_node.get_node("Exclusion")
+
+
+	if (body == tilemap1 or body == tilemap2):
+		queue_free()
+
+
+	pass
